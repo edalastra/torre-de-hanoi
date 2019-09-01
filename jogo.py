e30c1448c228414aead1be5602dbf7112c066b82
edalastra <elizandro.dalastra_B@compasso.com.br>
2019-09-01 10:31:58 -0300

    melhorando a view

--- jogo.py
+++ jogo.py
@@ -8,36 +8,54 @@ def controlador():
 
     while(not torres_de_hanoi.ganhou()):
         jogada(torres_de_hanoi)
-    print("Parabéns! Você venceu o desafio!")
+    imprime_mensagem_vencedor()
+    print('Numero de movimentos = {}'.format(torres_de_hanoi.movimentos))
+    if(torres_de_hanoi.movimentos > 15):
+        print('Voce pode fazer melhor. Tente novamente')
 
 
 def jogada(torres_de_hanoi):
     try:
-        de, para = input('Digite os respectivos números do movimento: ').split(' ')
+        de, para = input('Digite os respectivos numeros do movimento: ').split(' ')
         de = int(de)
         para = int(para)
+        os.system('cls' if os.name == 'nt' else 'clear')
         torres_de_hanoi.movimento(de, para)
         torres_de_hanoi.exibe()
 
     except (ValueError, IndexError):
-        print('Comandos inválidos digite novamente')
+        print('Comandos invalidos digite novamente')
         jogada(torres_de_hanoi)
-
-
-    os.system('cls' if os.name == 'nt' else 'clear')
 
     
 def welcome():
-    print('\t\tBem Vindo ao jogo Torre de Hanói\n')
-    print('\tEsse é um "quebra-cabeça" que consiste em uma base contendo três pinos,\n'
-          'em um dos quais são dispostos alguns discos uns sobre os outros,\n'
-          'em ordem crescente de diâmetro, de cima para baixo.\n\n'
-          'Objetivo: mover todos os discos para o pino da direita.\n'
-          'Regras: Digite primeiro o número da base que o disco está e o número\n'
-          'da base para qual ele será movido, você deve mover um disco de cada vez,\n'
-          'sendo que um disco maior nunca poderá ficar em cima de um disco menor.\n')
-    input('Tecle enter para começar...')
+    print("\t***************************************")
+    print("\t***Bem vindo ao jogo Torre de Hanoi!***")
+    print("\t***************************************")
+    print('\tEsse e um "quebra-cabeça" que consiste em uma base contendo tres \n'
+          'pinos, em um dos quais sao dispostos alguns discos uns sobre os outros,\n'
+          'em ordem crescente de diametro, de cima para baixo.\n\n'
+          '\tObjetivo: mover todos os discos para o pino da direita, com o numero de movimentos.\n'
+          'mais proximo de 15'
+          '\tRegras: Digite primeiro o numero da base que o disco esta e o numero\n'
+          'da base para qual ele sera movido, você deve mover um disco de cada vez,\n'
+          'sendo que um disco maior nunca podera ficar em cima de um disco menor.\n')
+    input('Tecle enter para comecar...')
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
 if(__name__ == "__main__"):
+    os.system('cls' if os.name == 'nt' else 'clear')
     controlador()
--- models/TorresDeHanoi.py
+++ models/TorresDeHanoi.py
@@ -3,16 +3,22 @@ from .Torre import Torre
 class TorresDeHanoi():
     def __init__(self):
         self.__torres = [Torre([1, 2, 3, 4]), Torre([]), Torre([])]
+        self.__movimentos = 0
 
     def movimento(self, de, para):
         if (not self.__torres[de - 1].vazia() and (
                 self.__torres[para - 1].vazia() or self.__torres[para - 1].ultimo() > self.__torres[de - 1].ultimo())):
             self.__torres[para - 1].empilha(self.__torres[de - 1].ultimo())
             self.__torres[de - 1].desempilha()
+            self.__movimentos += 1
         else:
             print('Movimento Invalido')
+    @property
+    def movimentos(self):
+        return self.__movimentos
 
     def exibe(self):
+        print('Torres: ')
         for i in range(0, 4):
             print("| {} | {} | {} |".format(self.__torres[0].toString[i],
                                             self.__torres[1].toString[i],
